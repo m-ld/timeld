ecbb5a13ad113625859f5c1ca5ffa6ceb1eaab1a
George Svarovsky <gsvarovsky@users.noreply.github.com>
2022-05-08 18:00:20 +0100

    Fixed locale-specificity in unit test

--- packages/cli/test/Entry.test.mjs
+++ packages/cli/test/Entry.test.mjs
@@ -3,6 +3,7 @@ import { Entry } from '../lib/Entry.mjs';
 describe('Timesheet entry', () => {
   test('from JSON', () => {
     // noinspection JSCheckFunctionSignatures
+    const startDateStr = '2022-05-06T10:24:22.139Z';
     const entry = Entry.fromJSON({
       '@id': 'session123/1',
       'session': { '@id': 'session123' },
@@ -10,7 +11,7 @@ describe('Timesheet entry', () => {
       'vf:provider': { '@id': 'https://alice.example/profile#me' },
       'start': {
         '@type': 'http://www.w3.org/2001/XMLSchema#dateTime',
-        '@value': '2022-05-06T10:24:22.139Z'
+        '@value': startDateStr
       },
       'duration': 60
     });
@@ -18,8 +19,9 @@ describe('Timesheet entry', () => {
     expect(entry.sessionId).toBe('session123');
     expect(entry.activity).toBe('testing');
     expect(entry.providerId).toBe('https://alice.example/profile#me');
-    expect(entry.start.toISOString()).toBe('2022-05-06T10:24:22.139Z');
+    expect(entry.start.toISOString()).toBe(startDateStr);
     expect(entry.duration).toBe(60);
-    expect(entry.toString()).toBe('#1: testing (5/6/2022, 11:24:22 AM, 1 hour)');
+    expect(entry.toString()).toMatch(
+      `#1: testing (${new Date(startDateStr).toLocaleString()}, 1 hour)`);
   });
 });
